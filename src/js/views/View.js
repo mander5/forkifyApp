72559b2b2995a3cb964dc89730553650715b45bd
mander5 <aaron_mander@hotmail.com>
2021-03-22 15:22:27 +0000

    Removing dist and cache,

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,6 +2,16 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
   _data;
+
+  /**
+   * Render the recieved object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to DOM
+   * @returns {undefined | string} A markup is returned if render = false
+   * @this {Object} View instance
+   * @author Aaron Mander
+   * @todo Finish implementation
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0)) {
       return this.renderError();
